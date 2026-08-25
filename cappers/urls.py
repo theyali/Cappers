61799dabe0b0083bf712dabde73e51e22bf3ad65
Ali Hajiyev <haciyev.ali@hotmail.com>
2026-08-25 18:38:48 +0400

    Done

--- cappers/urls.py
+++ cappers/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.http import JsonResponse
 from django.urls import include, path
 
@@ -19,4 +20,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
