574c5895add2721a1aba75b33dd36316276c30fe
Ali Hajiyev <83508265+theyali@users.noreply.github.com>
2026-08-26 04:03:40 +0400

    Add how it works route

--- front/urls.py
+++ front/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from . import home_views, views
+from . import home_views, how_views, views
 
 app_name = "front"
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path("", home_views.index, name="index"),
     path("predictions/", views.predictions, name="predictions"),
     path("cappers/", views.cappers_stats, name="cappers_stats"),
+    path("how-it-works/", how_views.how_it_works, name="how_it_works"),
 ]
