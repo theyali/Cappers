16d0b007c11aab4de7c25ae88e7995cdcaed623a
Ali Hajiyev <83508265+theyali@users.noreply.github.com>
2026-08-28 14:33:07 +0400

    Pass page ads into bookmaker sidebar

--- back/templatetags/site_extras.py
+++ back/templatetags/site_extras.py
@@ -10,11 +10,13 @@ from game.models import PredictionCoupon, PredictionItem
 register = template.Library()
 
 
-@register.inclusion_tag("back/_bookmakers_sidebar.html")
-def bookmakers_sidebar():
+@register.inclusion_tag("back/_bookmakers_sidebar.html", takes_context=True)
+def bookmakers_sidebar(context):
     return {
         "bookmakers": Bookmaker.objects.all(),
         "website_settings": WebsiteSettings.load(),
+        "adv_banners": context.get("adv_banners", []),
+        "adv_placement": context.get("adv_placement", "content"),
     }
 
 
