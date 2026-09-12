fa54d611b5a6f74309e5820f243be4f025a0c2b6
Ali Hajiyev <83508265+theyali@users.noreply.github.com>
2026-09-12 20:28:23 +0400

    Load roulette models with cabinet app

--- cabinet/apps.py
+++ cabinet/apps.py
@@ -8,4 +8,5 @@ class CabinetConfig(AppConfig):
 
     def ready(self) -> None:
         from . import presence  # noqa: F401
+        from . import roulette_models  # noqa: F401
         from . import signals  # noqa: F401
