2d8c67e30d46af1dd9c0aaba3a8c7e025653b3cc
Ali Hajiyev <83508265+theyali@users.noreply.github.com>
2026-09-13 16:14:34 +0400

    refactor: add cabinet roulette service package

---
+++ cabinet/roulette/__init__.py
@@ -0,0 +1,5 @@
+"""Roulette application services.
+
+Models intentionally remain in the parent ``cabinet`` package to preserve the
+existing Django migration/model paths while service code is being reorganized.
+"""
