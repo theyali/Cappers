58c4506b647d4b0e454f0aff123df1168372055e
Ali Hajiyev <83508265+theyali@users.noreply.github.com>
2026-09-15 17:18:31 +0400

    Register metrics models and signals

--- front/apps.py
+++ front/apps.py
@@ -4,4 +4,7 @@ from django.apps import AppConfig
 class FrontConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "front"
-    verbose_name = "Frontend сайта"
+
+    def ready(self) -> None:
+        from . import metric_models  # noqa: F401
+        from . import metric_signals  # noqa: F401
