b608163a74a9367cf850e9bc2e3f43819b494ef9
Ali Hajiyev <83508265+theyali@users.noreply.github.com>
2026-09-18 17:47:07 +0400

    Bonus center step 22: record daily visit on bonus page

--- cabinet/bonus_views.py
+++ cabinet/bonus_views.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from .models import DailyTask
 from .services.bonus_center import build_bonus_center_context
-from .services.streaks import touch_daily_streak
+from .services.daily_tasks import record_daily_task_action
 
 
 @login_required
 def bonuses(request):
-    touch_daily_streak(request.user)
+    record_daily_task_action(request.user, DailyTask.TaskType.DAILY_LOGIN)
     context = build_bonus_center_context(request.user, request=request)
     context.update(
         {
