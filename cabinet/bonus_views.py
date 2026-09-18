d707414dca8a558a1cc0f99145b96f5b81154d99
Ali Hajiyev <83508265+theyali@users.noreply.github.com>
2026-09-18 16:15:56 +0400

    Bonus center step 11: touch streak on bonus page

--- cabinet/bonus_views.py
+++ cabinet/bonus_views.py
@@ -2,10 +2,12 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from .services.bonus_center import build_bonus_center_context
+from .services.streaks import touch_daily_streak
 
 
 @login_required
 def bonuses(request):
+    touch_daily_streak(request.user)
     context = build_bonus_center_context(request.user, request=request)
     context.update(
         {
