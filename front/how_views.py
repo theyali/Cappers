0b8a0bfa8dd4de3080b1a5f04961a2a2d13cef27
Ali Hajiyev <haciyev.ali@hotmail.com>
2026-09-18 13:56:26 +0400

    Вщту

--- front/how_views.py
+++ front/how_views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def how_it_works(request):
-    return render(request, "front/how_it_works.html")
+    context={
+        'page_class':"how_it_works"
+    }
+    return render(request, "front/how_it_works.html", context)
